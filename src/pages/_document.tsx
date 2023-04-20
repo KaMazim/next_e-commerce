c5001f3a787f3c923c4f5e6c6e36097d9bf7f6a9
Kainã Mazim <kainamazim@gmail.com>
2023-04-20 13:04:26 -0300

    Initial Setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document: React.FC = () => {
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
