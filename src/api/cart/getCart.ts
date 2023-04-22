2977aed7f5f5e01555688c4a761c0c468174e884
Kainã Mazim <kainamazim@gmail.com>
2023-04-22 10:57:04 -0300

    feat: getCart api function

---
+++ src/api/cart/getCart.ts
@@ -0,0 +1,72 @@
+import api from '..';
+
+import getManyProducts from '../product/getManyProducts';
+
+import type { Product } from '../product/getProduct';
+
+export interface CartProduct extends Product {
+    quantity: number;
+}
+
+export interface Cart {
+    id: number;
+    userId: number;
+    date: Date;
+    products: CartProduct[];
+}
+
+interface CartProductResponse {
+    productId: number;
+    quantity: number;
+}
+
+interface CartResponse {
+    id: number;
+    userId: number;
+    date: string;
+    products: CartProductResponse[];
+}
+
+const getCartProducts = async (
+    productResponses: CartProductResponse[]
+): Promise<CartProduct[]> => {
+    const productIds = productResponses.map(({ productId }) => productId);
+    const products = await getManyProducts(productIds);
+
+    const cartProducts: CartProduct[] = products.map((product) => {
+        const index = productResponses.findIndex(
+            ({ productId }) => productId === product.id
+        );
+
+        const { quantity } = productResponses[index];
+
+        return { ...product, quantity };
+    });
+
+    return cartProducts;
+};
+
+const getCart = async (cartId: number): Promise<Cart | null> => {
+    const { data: response } = await api.get<CartResponse | undefined>(
+        `carts/${cartId}`
+    );
+
+    if (response?.id != null) {
+        const { id, userId, date } = response;
+
+        const cartProducts = await getCartProducts(response.products);
+
+        const cart: Cart = {
+            id,
+            userId,
+            date: new Date(date),
+            products: cartProducts
+        };
+
+        return cart;
+    }
+
+    return null;
+};
+
+export default getCart;
---
+++ test/unit/api/cart/getCart.test.ts
@@ -0,0 +1,49 @@
+import getCart from '../../../../src/api/cart/getCart';
+import type { Cart, CartProduct } from '../../../../src/api/cart/getCart';
+
+describe('getCart - API function', () => {
+    it('should return null when passed 0', async () => {
+        const expected = null;
+
+        const actual = await getCart(0);
+
+        expect(actual).toBe(expected);
+    });
+
+    it('should return null when Cart not found', async () => {
+        const expected = null;
+
+        const actual = await getCart(Infinity);
+
+        expect(actual).toBe(expected);
+    });
+
+    it('should return a Cart when passed an id', async () => {
+        const expectedId = 1;
+
+        const expected: Cart = {
+            id: expectedId,
+            userId: expect.any(Number),
+            date: expect.any(Date),
+            products: expect.arrayContaining<CartProduct>([
+                expect.objectContaining<CartProduct>({
+                    id: expect.any(Number),
+                    title: expect.any(String),
+                    price: expect.any(Number),
+                    description: expect.any(String),
+                    category: expect.any(String),
+                    image: expect.any(String),
+                    rating: {
+                        rate: expect.any(Number),
+                        count: expect.any(Number)
+                    },
+                    quantity: expect.any(Number)
+                })
+            ])
+        };
+
+        const actual = await getCart(expectedId);
+
+        expect(actual).toEqual(expect.objectContaining<Cart>(expected));
+    });
+});
